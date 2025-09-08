ab22c6263eac67e5415135f839ea158f24f4a91f
Jesse <jesse@lonocollective.ai>
2025-09-08 10:09:38 -0500

    remove docs folder

--- docs/src/scripts/main.js
+++
@@ -1,6 +0,0 @@
-import { initNavigation, initSmoothScrolling } from './components/navigation.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    initNavigation();
-    initSmoothScrolling();
-});
